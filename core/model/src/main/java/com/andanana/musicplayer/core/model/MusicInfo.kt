5bc48fe1e65b6c60b91eaa06d15066ca51a09022
andannn <103762629+andannn@users.noreply.github.com>
2023-02-08 23:21:55 +0800

    parcel

--- core/model/src/main/java/com/andanana/musicplayer/core/model/MusicInfo.kt
+++ core/model/src/main/java/com/andanana/musicplayer/core/model/MusicInfo.kt
@@ -1,6 +1,8 @@
 package com.andanana.musicplayer.core.model
 
 import android.net.Uri
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.media3.common.MediaItem
 
 data class MusicInfo(
@@ -14,6 +16,47 @@ data class MusicInfo(
     val album: String = "",
     val artist: String = "",
     val albumUri: Uri = Uri.parse("")
-) {
+) : Parcelable {
+
     val mediaItem: MediaItem = MediaItem.fromUri(contentUri)
+
+    constructor(parcel: Parcel) : this(
+        contentUri = parcel.readParcelable(Uri::class.java.classLoader) ?: Uri.EMPTY,
+        title = parcel.readString() ?: "",
+        duration = parcel.readInt(),
+        modifiedDate = parcel.readLong(),
+        size = parcel.readInt(),
+        mimeType = parcel.readString() ?: "",
+        absolutePath = parcel.readString() ?: "",
+        album = parcel.readString() ?: "",
+        artist = parcel.readString() ?: "",
+        albumUri = parcel.readParcelable(Uri::class.java.classLoader) ?: Uri.EMPTY
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeParcelable(contentUri, flags)
+        parcel.writeString(title)
+        parcel.writeInt(duration)
+        parcel.writeLong(modifiedDate)
+        parcel.writeInt(size)
+        parcel.writeString(mimeType)
+        parcel.writeString(absolutePath)
+        parcel.writeString(album)
+        parcel.writeString(artist)
+        parcel.writeParcelable(albumUri, flags)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MusicInfo> {
+        override fun createFromParcel(parcel: Parcel): MusicInfo {
+            return MusicInfo(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MusicInfo?> {
+            return arrayOfNulls(size)
+        }
+    }
 }
