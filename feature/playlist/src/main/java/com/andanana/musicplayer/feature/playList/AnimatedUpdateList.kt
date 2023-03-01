bc64c43216e52be537db009c5e0bf67b6d4dbd0e
andannn <103762629+andannn@users.noreply.github.com>
2023-03-01 22:01:47 +0800

    AnimatedList

--- feature/playlist/src/main/java/com/andanana/musicplayer/feature/playList/AnimatedUpdateList.kt
+++ feature/playlist/src/main/java/com/andanana/musicplayer/feature/playList/AnimatedUpdateList.kt
@@ -1,6 +1,5 @@
 package com.andanana.musicplayer.feature.playList
 
-import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.animation.expandVertically
@@ -8,26 +7,23 @@ import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.andanana.musicplayer.core.designsystem.component.MusicCard
 import com.andanana.musicplayer.core.model.MusicInfo
-import kotlinx.coroutines.delay
 
 private const val TAG = "AnimatedUpdateList"
 
 @Composable
 fun AnimatedUpdateList(
     modifier: Modifier = Modifier,
-    list: List<MusicInfo>
+    list: List<MusicInfo>,
+    content: @Composable () -> Unit
 ) {
     val listState = remember {
         AnimatedListState()
@@ -35,22 +31,13 @@ fun AnimatedUpdateList(
     LaunchedEffect(list) {
         listState.submitList(list)
     }
-    LaunchedEffect(Unit) {
-        while (true) {
-            delay(1000)
-            listState.submitList(
-                listState._list.toMutableList().apply { removeAt(0) }.map { it.info }.also {
-                    Log.d(TAG, "AnimatedUpdateList: ${it.map { it.title }}")
-                }
-            )
-        }
-    }
+
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(horizontal = 20.dp, vertical = 10.dp)
     ) {
         items(
-            items = listState._list,
+            items = listState.list,
             key = { it.hashCode() }
         ) { item ->
             val info = item.info
@@ -60,29 +47,15 @@ fun AnimatedUpdateList(
                 enter = expandVertically() + fadeIn(),
                 exit = shrinkVertically() + fadeOut()
             ) {
-                MusicCard(
-                    modifier = Modifier.padding(vertical = 4.dp),
-                    colors = CardDefaults.cardColors(),
-                    albumArtUri = info.albumUri,
-                    title = info.title,
-                    showTrackNum = false,
-                    artist = info.artist,
-                    trackNum = info.cdTrackNumber,
-                    date = info.modifiedDate,
-                    onMusicItemClick = {
-//                    onAudioItemClick(musicItems, musicItems.indexOf(info))
-                    },
-                    onOptionButtonClick = {
-//                    onShowMusicItemOption(info.contentUri)
-                    }
-                )
+                content()
             }
         }
     }
 }
 
 private class AnimatedListState {
-    val _list: MutableList<ListItem> = mutableStateListOf()
+    private val _list: MutableList<ListItem> = mutableStateListOf()
+    val list: List<ListItem> = _list
 
     class ListItem(
         val visibleState: MutableTransitionState<Boolean>,
@@ -104,16 +77,22 @@ private class AnimatedListState {
         )
         val newSortedList = map.toSortedMap().map { it.value }
 
-        val iterator = _list.listIterator()
-//        while (iterator.hasNext()) {
-//            val item = iterator.next()
-//            if (!newSortedList.contains(item)) {
-//                item.visibleState.targetState = false
-//            }
-//        }
-        newSortedList.forEach {
-            val item = iterator.next()
-
+        // Remove item.
+        run {
+            val iterator = _list.listIterator()
+            while (iterator.hasNext()) {
+                val item = iterator.next()
+                if (!newSortedList.contains(item)) {
+                    item.visibleState.targetState = false
+                }
+            }
         }
+
+        // Add item.
+        val diffItems = newSortedList.minus(_list.toSet())
+        _list.addAll(diffItems)
+
+        // Sort the list by order.
+        _list.sortBy { newSortedList.indexOf(it) }
     }
 }
