3aa1428e4ddb3cd6b48c72025b0922b1bb3f6685
andannn <103762629+andannn@users.noreply.github.com>
2023-03-12 22:55:43 +0900

    library pager

--- feature/home/src/main/java/com/andanana/musicplayer/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/andanana/musicplayer/feature/home/HomeScreen.kt
@@ -1,31 +1,16 @@
 package com.andanana.musicplayer.feature.home
 
 import android.net.Uri
-import android.util.Log
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.andanana.musicplayer.core.designsystem.component.TabRowAndPager
 import com.andanana.musicplayer.core.model.MusicInfo
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.rememberPagerState
-import kotlinx.coroutines.launch
 
 private const val TAG = "HomeScreen"
 
@@ -44,7 +29,6 @@ fun HomeRoute(
     )
 }
 
-@OptIn(ExperimentalPagerApi::class)
 @Composable
 private fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -52,52 +36,17 @@ private fun HomeScreen(
     onNavigateToPlayList: (Uri) -> Unit,
     onShowMusicItemOption: (Uri) -> Unit
 ) {
-    val scope = rememberCoroutineScope()
-    val pageState = rememberPagerState()
-    val pageIndex = remember(key1 = pageState) {
-        pageState.currentPage
-    }
-    var selectedPageIndex by remember { mutableStateOf(pageIndex) }
-    val homePage = remember(selectedPageIndex) { HomePage.values()[selectedPageIndex] }
-
-    LaunchedEffect(Unit) {
-        snapshotFlow {
-            pageState.currentPage
-        }.collect { index ->
-            Log.d(TAG, "HomeScreen: ")
-            selectedPageIndex = index
-        }
-    }
-
-    Column(
-        modifier = modifier.fillMaxSize()
-    ) {
-        TabRow(
-            modifier = Modifier.fillMaxWidth(),
-            selectedTabIndex = selectedPageIndex
-        ) {
-            HomePage.values().forEach { page ->
-                Tab(
-                    selected = page == homePage,
-                    onClick = {
-                        scope.launch {
-                            pageState.animateScrollToPage(page.ordinal)
-                        }
-                    }
-                ) {
-                    Text(
-                        modifier = modifier.padding(vertical = 10.dp),
-                        text = stringResource(id = page.titleResId)
-                    )
-                }
-            }
-        }
-
-        HorizontalPager(
-            count = HomePage.values().size,
-            state = pageState
-        ) { index ->
-            when (HomePage.values()[index]) {
+    TabRowAndPager(
+        modifier = modifier.fillMaxSize(),
+        items = HomePage.values().toList(),
+        tabRowContent = { page ->
+            Text(
+                modifier = modifier.padding(vertical = 10.dp),
+                text = stringResource(id = page.titleResId)
+            )
+        },
+        pagerContent = { page ->
+            when (page) {
                 HomePage.AUDIO_PAGE -> {
                     AudioPage(
                         Modifier.fillMaxSize(),
@@ -115,7 +64,7 @@ private fun HomeScreen(
                 )
             }
         }
-    }
+    )
 }
 
 enum class HomePage(
