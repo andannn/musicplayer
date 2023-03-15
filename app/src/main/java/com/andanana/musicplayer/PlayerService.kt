26cf5d435f88331c29b9cc95382a54b8277f3701
andannn <103762629+andannn@users.noreply.github.com>
2023-03-15 22:05:26 +0900

    app notification

--- app/src/main/java/com/andanana/musicplayer/PlayerService.kt
+++ app/src/main/java/com/andanana/musicplayer/PlayerService.kt
@@ -1,16 +1,11 @@
 package com.andanana.musicplayer
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.annotation.RequiresApi
-import androidx.core.app.NotificationCompat
 import com.andanana.musicplayer.core.player.repository.PlayerRepository
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -21,6 +16,9 @@ class PlayerService : Service() {
     @Inject
     lateinit var playerRepository: PlayerRepository
 
+    @Inject
+    lateinit var smpNotificationManager: SmpNotificationManager
+
     private lateinit var mediaSession: MediaSessionCompat
 
     override fun onBind(intent: Intent?): IBinder? = null
@@ -30,21 +28,10 @@ class PlayerService : Service() {
         playerRepository.initial()
 
         mediaSession = MediaSessionCompat(this, MEDIA_SESSION)
-        val requiredChannels = listOf(
-            NotificationChannel(
-                PLAYER_SERVICE,
-                "Player",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-        )
-        val notificationManager =
-            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notificationManager.createNotificationChannels(requiredChannels)
 
         startForeground(
             10,
-            getPlayerNotification(
-                context = this,
+            smpNotificationManager.getPlayerNotification(
                 session = mediaSession
             )
         )
@@ -54,30 +41,6 @@ class PlayerService : Service() {
 
     override fun onDestroy() {
         playerRepository.release()
-    }
-
-    private fun getPlayerNotification(
-        context: Context,
-        session: MediaSessionCompat
-    ): Notification {
-        val mediaStyle = androidx.media.app.NotificationCompat.MediaStyle()
-            .setShowActionsInCompactView(1, 2, 3)
-            .setMediaSession(session.sessionToken)
-        return NotificationCompat.Builder(context, PLAYER_SERVICE).apply {
-            setSmallIcon(R.mipmap.ic_launcher)
-            setContentTitle("Now Playing")
-            setContentText("")
-            setOngoing(true)
-            priority = NotificationCompat.PRIORITY_MAX
-            setSilent(true)
-            setStyle(mediaStyle)
-//        addAction(if (isLiked) filledLikeAction else outlinedLikeAction)
-//        addAction(previousAction)
-//        addAction(if (showPlayButton) playAction else pauseAction)
-//        addAction(nextAction)
-//        addAction(cancelAction)
-//        setContentIntent(activityIntent)  // use with android:launchMode="singleTask" in manifest
-        }.build()
     }
 
     companion object {
