0503e9c680a133c02672da775fc09fedc85474c8
andannn <103762629+andannn@users.noreply.github.com>
2023-03-25 10:15:59 +0900

    cancel animation

--- core/designsystem/src/main/java/com/andanana/musicplayer/core/designsystem/component/AnimatedUpdateList.kt
+++ core/designsystem/src/main/java/com/andanana/musicplayer/core/designsystem/component/AnimatedUpdateList.kt
@@ -1,20 +1,9 @@
 package com.andanana.musicplayer.core.designsystem.component
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.MutableTransitionState
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -26,76 +15,51 @@ fun <K : Any> AnimatedUpdateList(
     list: List<K>,
     content: @Composable (K) -> Unit
 ) {
-    val listState = remember {
-        AnimatedListState<K>()
-    }
-    LaunchedEffect(list) {
-        listState.submitList(list)
-    }
-
     LazyColumn(
         modifier = modifier,
         contentPadding = PaddingValues(horizontal = 20.dp, vertical = 10.dp)
     ) {
         items(
-            items = listState.list,
-            key = { it.first }
+            items = list,
+            key = { it }
         ) { item ->
-            val visibleState = item.second
-            AnimatedVisibility(
-                visibleState,
-                enter = slideInHorizontally(
-                    initialOffsetX = { -it / 2 },
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioLowBouncy,
-                        stiffness = Spring.StiffnessLow
-                    )
-                ) + fadeIn(),
-                exit = slideOutHorizontally(
-                    animationSpec = spring(
-                        dampingRatio = Spring.DampingRatioLowBouncy,
-                        stiffness = Spring.StiffnessLow
-                    )
-                ) + fadeOut()
-            ) {
-                content(item.first)
-            }
+            content(item)
         }
     }
 }
 
-private class AnimatedListState<K> {
-    private val _list: MutableList<Pair<K, MutableTransitionState<Boolean>>> = mutableStateListOf()
-    val list: List<Pair<K, MutableTransitionState<Boolean>>> = _list
-
-    fun submitList(newList: List<K>) {
-        val map = newList.associateBy(
-            keySelector = {
-                newList.indexOf(it)
-            },
-            valueTransform = {
-                _list.find { item -> item.first == it }
-                    ?: (it to MutableTransitionState(false).apply { targetState = true })
-            }
-        )
-        val newSortedList = map.toSortedMap().map { it.value }
-
-        // Remove item.
-        run {
-            val iterator = _list.listIterator()
-            while (iterator.hasNext()) {
-                val item = iterator.next()
-                if (!newSortedList.contains(item)) {
-                    item.second.targetState = false
-                }
-            }
-        }
-
-        // Add item.
-        val diffItems = newSortedList.minus(_list.toSet())
-        _list.addAll(diffItems)
-
-        // Sort the list by order.
-        _list.sortBy { newSortedList.indexOf(it) }
-    }
-}
+// private class AnimatedListState<K> {
+//    private val _list: MutableList<Pair<K, MutableTransitionState<Boolean>>> = mutableStateListOf()
+//    val list: List<Pair<K, MutableTransitionState<Boolean>>> = _list
+//
+//    fun submitList(newList: List<K>) {
+//        val map = newList.associateBy(
+//            keySelector = {
+//                newList.indexOf(it)
+//            },
+//            valueTransform = {
+//                _list.find { item -> item.first == it }
+//                    ?: (it to MutableTransitionState(false).apply { targetState = true })
+//            }
+//        )
+//        val newSortedList = map.toSortedMap().map { it.value }
+//
+//        // Remove item.
+//        run {
+//            val iterator = _list.listIterator()
+//            while (iterator.hasNext()) {
+//                val item = iterator.next()
+//                if (!newSortedList.contains(item)) {
+//                    item.second.targetState = false
+//                }
+//            }
+//        }
+//
+//        // Add item.
+//        val diffItems = newSortedList.minus(_list.toSet())
+//        _list.addAll(diffItems)
+//
+//        // Sort the list by order.
+//        _list.sortBy { newSortedList.indexOf(it) }
+//    }
+// }
