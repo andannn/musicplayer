bd0e0706c83314d3bad7b2c862019e645af8e79f
dhc-jiangqingnan <qingnan.jiang@dhc.com.cn>
2024-02-06 13:45:34 +0800

    upgradse ksp to 1.9.22-1.0.17

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.configurationcache.extensions.capitalized
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("musicplayer.android.library")
     id("musicplayer.android.hilt")
@@ -21,6 +24,11 @@ dependencies {
     implementation(libs.protobuf.kotlin.lite)
 }
 
+
+// TODO: Remove this after problem solved.
+// Could not build this module when upgrade ksp to 1.9.22-1.0.17.
+// find this workaround in github:
+// https://github.com/google/ksp/issues/1590
 protobuf {
     protoc {
         artifact = libs.protobuf.protoc.get().toString()
@@ -34,6 +42,16 @@ protobuf {
                 register("kotlin") {
                     option("lite")
                 }
+            }
+        }
+    }
+}
+androidComponents {
+    onVariants(selector().all()) { variant ->
+        afterEvaluate {
+            val capName = variant.name.capitalized()
+            tasks.getByName<KotlinCompile>("ksp${capName}Kotlin") {
+                setSource(tasks.getByName("generate${capName}Proto").outputs)
             }
         }
     }
