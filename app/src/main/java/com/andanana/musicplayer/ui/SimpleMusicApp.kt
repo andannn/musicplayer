83aa56875162bfe5d7b32f492f57c0b275578523
andannn <jqn296763005@gmail.com>
2024-02-15 18:39:54 +0900

    add padding for NavHost when playing

--- app/src/main/java/com/andanana/musicplayer/ui/SimpleMusicApp.kt
+++ app/src/main/java/com/andanana/musicplayer/ui/SimpleMusicApp.kt
@@ -1,23 +1,69 @@
 package com.andanana.musicplayer.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.andanana.musicplayer.feature.player.PlayerSheet
+import com.andanana.musicplayer.feature.player.PlayerStateViewModel
+import com.andanana.musicplayer.feature.player.PlayerUiState
+import com.andanana.musicplayer.feature.player.widget.PlayerShrinkHeight
 import com.andanana.musicplayer.navigation.SmpNavHost
 
 @Composable
-fun SimpleMusicApp(modifier: Modifier = Modifier) {
+fun SimpleMusicApp(
+    modifier: Modifier = Modifier,
+    playerStateViewModel: PlayerStateViewModel = hiltViewModel(),
+) {
     Surface(modifier = modifier.fillMaxSize()) {
-        val navController = rememberNavController()
+        val state by playerStateViewModel.playerUiStateFlow.collectAsState()
+
+        val isPlaying by remember {
+            derivedStateOf {
+                state is PlayerUiState.Active
+            }
+        }
+
+        SmpNavHostContainer(
+            modifier = Modifier.fillMaxSize(),
+            isPlaying = isPlaying,
+        )
+
+        if (state is PlayerUiState.Active) {
+            PlayerSheet(
+                state = state as PlayerUiState.Active,
+                onEvent = playerStateViewModel::onEvent,
+            )
+        }
+    }
+}
+
+@Composable
+fun SmpNavHostContainer(
+    isPlaying: Boolean,
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController(),
+) {
+    Column(modifier = modifier) {
         SmpNavHost(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().weight(1f),
             navHostController = navController,
         )
 
-        PlayerSheet()
+        if (isPlaying) {
+            Spacer(modifier = Modifier.navigationBarsPadding().height(PlayerShrinkHeight))
+        }
     }
 }
--- feature/player/src/main/java/com/andanana/musicplayer/feature/player/PlayerSheet.kt
+++ feature/player/src/main/java/com/andanana/musicplayer/feature/player/PlayerSheet.kt
@@ -1,19 +1,15 @@
 package com.andanana.musicplayer.feature.player
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.andanana.musicplayer.feature.player.widget.ShrinkablePlayBox
 
 @Composable
-fun PlayerSheet(playerStateViewModel: PlayerStateViewModel = hiltViewModel()) {
-    val state by playerStateViewModel.playerUiStateFlow.collectAsState()
-
-    if (state is PlayerUiState.Active) {
-        ShrinkablePlayBox(
-            state = state as PlayerUiState.Active,
-            onEvent = playerStateViewModel::onEvent,
-        )
-    }
+fun PlayerSheet(
+    state: PlayerUiState.Active,
+    onEvent: (PlayerUiEvent) -> Unit,
+) {
+    ShrinkablePlayBox(
+        state = state,
+        onEvent = onEvent,
+    )
 }
