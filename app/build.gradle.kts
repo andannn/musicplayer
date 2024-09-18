2bbe0f55c8a347e8f0a89d6d8889a38a019a5f2d
andannn <jqn296763005@gmail.com>
2024-09-18 23:27:22 +0900

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "com.andanana.musicplayer"
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.0.1-alpha7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
