1b787b3080b9333e1f65a805807594491939b49f
andannn <jqn296763005@gmail.com>
2024-10-07 15:11:54 +0900

    permission

--- composeApp/src/iosMain/kotlin/com/andannn/melodify/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/andannn/melodify/MainViewController.kt
@@ -1,9 +1,15 @@
 package com.andannn.melodify
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.ComposeUIViewController
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import org.koin.core.context.startKoin
+import platform.MediaPlayer.MPMediaLibrary
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
@@ -15,5 +21,30 @@ fun MainViewController() = ComposeUIViewController(
         }
     }
 ) {
-    MelodifyApp()
+    var permissionGranted by remember {
+        mutableStateOf(haveMediaPermission())
+    }
+    if (!permissionGranted) {
+        LaunchedEffect(Unit) {
+            MPMediaLibrary.requestAuthorization { status ->
+                permissionGranted = haveMediaPermission()
+            }
+        }
+    }
+
+    Napier.d("Permission granted: $permissionGranted")
+    if (permissionGranted)  {
+        MelodifyApp()
+    }
+}
+
+private fun haveMediaPermission(): Boolean {
+    return MPMediaLibrary.authorizationStatus() == MPMediaLibraryAuthorizationStatus.authorized.ordinal.toLong()
+}
+
+private enum class MPMediaLibraryAuthorizationStatus {
+    notDetermined,
+    restricted,
+    denied,
+    authorized
 }
