f532481043aafe89eb7bca2da233583a5e8cd9a9
andannn <jqn296763005@gmail.com>
2024-10-07 14:03:14 +0900

    debug log

--- composeApp/src/iosMain/kotlin/com/andannn/melodify/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/andannn/melodify/MainViewController.kt
@@ -1,10 +1,15 @@
 package com.andannn.melodify
 
 import androidx.compose.ui.window.ComposeUIViewController
+import io.github.aakira.napier.DebugAntilog
+import io.github.aakira.napier.Napier
 import org.koin.core.context.startKoin
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
+// TODO: Check debug build
+        Napier.base(DebugAntilog())
+
         startKoin {
             modules(modules)
         }
